a56c8a6116a4c629981f3d87c59f23eb70c11084
Стас Басов <stas.basov@whatever.com>
2023-01-24 14:33:23 +0300

    New File

--- main.py
+++ main.py
@@ -1,26 +1,74 @@
-from module import run_game
+# .../Dev/character_creation_module/main.py
+# Импортируем функцию стандартного модуля random.
+from random import randint
 
-INTRO = '''РАССЧИТАЙ И ПОБЕДИ!
-Загрузка...
+# Вот она — новая глобальная константа.
+DEFAULT_ATTACK = 5
+# Новая константа — стандартное значение защиты.
+DEFAULT_DEFENCE = 10
+DEFAULT_STAMINA = 80
 
-Твоя цель — за 5 ходов набрать такое количество очков урона противнику,
-которое попадет в диапазон +– 10 от значения здоровья противника.
+class Character:
+    # Константа для диапазона очков урона.
+    RANGE_VALUE_ATTACK = (1, 3)
+    # Новая переменная класса — диапазон значения защиты.
+    RANGE_VALUE_DEFENCE = (1, 5)
+    # Вот они — две новые константы.
+    SPECIAL_BUFF = 15
+    SPECIAL_SKILL = 'Удача'
+    BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
 
-Значение здоровья противника генерируется случайным образом
-в диапазоне от 80 до 120 очков.
+    # Объявляем конструктор класса.
+    def __init__(self, name):
+        self.name = name
+    
+   # Объявляем метод атаки
+    def attack(self):
+        # Вместо числа 5 теперь используется константа DEFAULT_ATTACK.
+        value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
+        return (f'{self.name} нанёс противнику урон, равный {value_attack}')
 
-В твоём распоряжении три вида атак:
-lite — урон от 2 до 5 очков;
-mid — урон от 15 до 25 очков;
-hard — урон от 30 до 40 очков.
-ВПЕРЁД К ПОБЕДЕ!!!
-'''
+    # Объявляем метод защиты.
+    def defence(self):
+        # Вычисляем значение защиты в переменной value_defence.
+        value_defence = DEFAULT_DEFENCE + randint(*self.RANGE_VALUE_DEFENCE)
+        return (f'{self.name} блокировал {value_defence} ед. урона.')
+    
+    # Объявляем метод специального умения.
+    def special(self):
+        # Здесь описано тело метода special().
+        return (f'{self.name} применил специальное умение '
+                f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}".')
 
-def main():
-    print(INTRO)
-    replay = True
-    while replay:
-        replay = run_game()
+    # Новый метод базового класса.
+    def __str__(self):
+        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.'
 
-if __name__ == '__main__':
-    main()
+class Warrior(Character):
+    BRIEF_DESC_CHAR_CLASS = (' дерзкий воин ближнего боя. '
+                             'Сильный, выносливый и отважный')
+    RANGE_VALUE_ATTACK = (3, 5)
+    RANGE_VALUE_DEFENCE = (5, 10)
+    SPECIAL_BUFF = DEFAULT_STAMINA + 25
+    SPECIAL_SKILL = 'Выносливость'
+
+class Mage(Character):
+    BRIEF_DESC_CHAR_CLASS = (' находчивый воин дальнего боя. '
+                             'Обладает высоким интеллектом')
+    RANGE_VALUE_ATTACK = (5, 10)
+    RANGE_VALUE_DEFENCE = (-2, 2)
+    SPECIAL_BUFF = DEFAULT_ATTACK + 40
+    SPECIAL_SKILL = 'Атака'
+
+class Healer(Character):
+    BRIEF_DESC_CHAR_CLASS = (' могущественный заклинатель. '
+                             'Черпает силы из природы, веры и духов')
+    RANGE_VALUE_ATTACK = (-3, -1)
+    RANGE_VALUE_DEFENCE = (2, 5)
+    SPECIAL_BUFF = DEFAULT_DEFENCE + 30
+    SPECIAL_SKILL = 'Защита'
+
+
+warrior = Warrior('Кодослав')
+print(warrior)
+print(warrior.attack())
